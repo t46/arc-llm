b995aa2d7cb0317c83b50282e0cf81a657c7577a
Ippei Fujisawa <fujisawa@araya.org>
2024-07-31 15:18:40 +0900

    introduce class Conversation and tidy codes

--- run_larc.py
+++ run_larc.py
@@ -1,44 +1,31 @@
 # the experiment ! get gpt4 in here
 import os
-from openai import OpenAI
 import json
-import numpy as np
+from pathlib import Path
 
-# OpenAI の ver 1.0 以降でLLMの出力の取得
-api_key_path = "../../openai_po991_arc.key"
-with open(api_key_path) as f:
-    api_key = f.readline()[:-1]
-os.environ["OPENAI_API_KEY"] = api_key
+import numpy as np
+from openai import OpenAI
+
+# get API key
+api_key_path = Path("../../openai_po991_arc.key")
+os.environ["OPENAI_API_KEY"] = api_key_path.read_text().strip()
 client = OpenAI()
 
-
-def get_llm_response(conversation):
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=conversation,
-    )
-    return response.choices[0].message.content
-
-
-def eval_score(pred_grid, gt_grid):
-    print(pred_grid)
-    print(gt_grid)
-
+def eval_score(pred_grid, gt_grid, show=True):
     pred_grid = np.array(eval(str(pred_grid)))
     gt_grid = np.array(eval(str(gt_grid)))
-
     pred_shape = pred_grid.shape
     gt_shape = gt_grid.shape
 
-    print(pred_shape)
-    print(gt_shape)
-
     if pred_shape != gt_shape:
-        return 0
+        score = 0.0
     else:
         score = (pred_grid == gt_grid).sum() / (pred_shape[0] * pred_shape[1])
-        return score
 
+    if show:
+        print("\033[93m" + f"score: {score}" + "\033[0m")
+
+    return score
 
 preamble = """
 lets play a game where you are transforming an input grid of numbers into an output grid of numbers
@@ -103,48 +90,87 @@ Output gird surrounded by <output_grid> and <output_grid>
     return prompt
 
 
-if __name__ == "__main__":
+def get_llm_response(conversation):
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=conversation,
+    )
+    return response.choices[0].message.content
 
-    max_num_tasks = 1
-
-    # open data/larc_gpt4.json
-    with open("data/larc_gpt4.json") as json_file:
-        larc_gpt4 = json.load(json_file)
-
-    print(len(larc_gpt4))
-    for task in larc_gpt4[:max_num_tasks]:
-
-        conversation = [
+class Coversation:
+    def __init__(self, save_dir):
+        self.conversation = [
             {"role": "system", "content": "You are an autonomous task solver."}
         ]
-        prompt = nl_and_io_prompt(task)
-        conversation.append({"role": "user", "content": prompt})
+        self.roles = ['system']
+        self.save_dir = save_dir
 
-        # print(prompt)
-        answer = get_llm_response(conversation)
-        # print(answer)
-        conversation.append({"role": "assistant", "content": answer})
+    def add_user(self, txt):
+        self.conversation.append({"role": "user", "content": txt})
+        self.roles.append("user")
 
-        for round in range(3):
+    def add_assistant(self, txt):
+        self.conversation.append({"role": "assistant", "content": txt})
+        self.roles.append("assistant")
+
+    @property
+    def history(self):
+        return self.conversation
+    
+    def _print(self, conv):
+        print("=" * 20)
+        print(f"[{conv['role']}]")
+        print(conv['role'])
+        print("=" * 20)
+        print(conv["content"])
+        print('-*' * 40)
+
+    def print(self):
+        conv = self.conversation[-1]
+        self._print(conv)
+
+    def print_history(self):
+        for conv in self.conversation:
+            self._print(conv)
+    
+    def save_conversation(self):
+        files = save_dir.glob("*.json")
+        path = self.save_dir / f'{str(len(list(files))).zfill(3)}.json'
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with open(path, "w") as f:
+            json.dump(self.conversation, f, indent=4)
+
+if __name__ == "__main__":
+    save_dir = Path('./result')
+
+    with open("data/larc_gpt4.json") as json_file:
+        larc_gpt4 = json.load(json_file)
+    selected_tasks = larc_gpt4[:1]
+
+    for task in selected_tasks:
+        conv = Coversation(save_dir=save_dir)
+        conv.add_user(nl_and_io_prompt(task))
+        conv.print()
+        conv.add_assistant(get_llm_response(conv.history))
+        conv.print()
+
+        scores = []
+        for round in range(2):
             print(f"Round {round}")
-            prompt = review(task["problem"]["train"][1]["output"])
-            conversation.append({"role": "user", "content": prompt})
-
-            answer = get_llm_response(conversation)
-            conversation.append({"role": "assistant", "content": answer})
+            conv.add_user(review(task["problem"]["train"][1]["output"]))
+            conv.print()
+            answer = get_llm_response(conv.history)
+            conv.add_assistant(answer)
+            conv.print()
 
             pred_grid = answer.split("<output_grid>")[1].split("</output_grid>")[0]
             gt_grid = task["problem"]["train"][1]["output"]
 
             score = eval_score(pred_grid, gt_grid)
-            print("\033[93m" + f"score: {score}" + "\033[0m")
-
+            scores.append(score)
             if score == 1:
+                print("Get the correct answer!")
+                print("Break the round loop")
                 break
-
-        task["prediction"] = pred_grid
-
-        for conv in conversation:
-            print("-" * 10)
-            print(f"[{conv['role']}]")
-            print(conv["content"])
+        
+        conv.save_conversation()
